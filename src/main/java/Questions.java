1b20eeb316a3365e93082a8fc9c9e28082935387
1GodLess5 <gacekalbert@seznam.cz>
2024-01-17 20:57:36 +0100

    updated class Questions -> new parameter topic

--- src/main/java/Questions.java
+++ src/main/java/Questions.java
@@ -1,16 +1,22 @@
 import java.util.HashSet;
 
 public class Questions {
+    private String topic;
     private String question;
     private HashSet<String> answers = new HashSet<>();
     private int numberOfCorrectAnswers;
     private HashSet<String> correctAnswers = new HashSet<>();
 
-    public Questions(String question, HashSet<String> answers, int numberOfCorrectAnswers, HashSet<String> correctAnswers){
+    public Questions(String topic, String question, HashSet<String> answers, int numberOfCorrectAnswers, HashSet<String> correctAnswers){
+        this.topic = topic;
         this.question = question;
         this.answers = answers;
         this.numberOfCorrectAnswers = numberOfCorrectAnswers;
         this.correctAnswers = correctAnswers;
+    }
+
+    public String getTopic(){
+        return this.topic;
     }
 
     public String getQuestion() {
