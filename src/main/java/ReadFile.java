c566a36f08612e5cb8ab04e6eb626519536eab0c
1GodLess5 <gacekalbert@seznam.cz>
2024-01-17 20:40:50 +0100

    reworked class ReadFile -> method read now reads file, reformats strings and adds Questions object, returns HashSet

--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -1,29 +1,34 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
-import java.util.Scanner;
 
 public class ReadFile {
-    private static HashSet<String> read(){
-        HashSet<String> fileText = new HashSet<>();
-        try {
-            File file = new File("src/main/java/questions.txt");
-            Scanner scanner = new Scanner(file);
+    public static HashSet<Questions> read(){
+        HashSet<Questions> questions = new HashSet<>();
 
-            while (scanner.hasNextLine()){
-                if (!scanner.nextLine().equals("\n")){
-                    fileText.add(scanner.nextLine().strip());
+        try (BufferedReader scanner = new BufferedReader(new FileReader("src/main/java/questions.txt"))) {
+            String line;
+
+            while ((line = scanner.readLine()) != null) {
+                String[] parts = line.split("[\\-!]");
+                HashSet<String> answers = new HashSet<>();
+                HashSet<String> correctAnswers = new HashSet<>();
+                String question = parts[0];
+                for (int i = 1; i < 5; i++){
+                    answers.add(parts[i]);
                 }
-            }
-            scanner.close();
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
-        }
-        System.out.println(fileText);
-        return fileText;
-    }
 
-//    private static HashSet<String> formatDecode(){
-//
-//    }
+                for (int i = 5; i < parts.length; i++){
+                    correctAnswers.add(parts[i]);
+                }
+                Questions questionObject = new Questions(question, answers, correctAnswers.size(), correctAnswers);
+                questions.add(questionObject);
+            }
+        } catch (IOException e) {
+            System.out.println("Oops, something went wrong.");
+        }
+
+        return questions;
+    }
 }
