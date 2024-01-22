60bbe2fdce81cc9247bd614b53a3c0cc11bf45d5
1GodLess5 <gacekalbert@seznam.cz>
2024-01-22 19:48:34 +0100

    refactor for different classes

--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -6,9 +6,8 @@ import java.util.*;
 public class ReadFile {
     private final static String fileName = "src/main/java/questions.txt";
 
-    public static List<Quiz> read(){
+    public static List<Quiz> read() {
         List<Quiz> quizzes = new ArrayList<>();
-        String oldTopic = "General Knowledge";
         int quizOrder = 0;
 
         try (BufferedReader scanner = new BufferedReader(new FileReader(fileName))) {
@@ -16,34 +15,35 @@ public class ReadFile {
 
             while ((line = scanner.readLine()) != null) {
                 String[] parts = line.split("[-!]");
-
-                List<Question> questions = new ArrayList<>();
-                List<Answer> answers = new ArrayList<>();
-
                 String questionTopic = parts[0];
                 String question = parts[1];
 
-                for (int i = 2; i < 6; i++){
-                    boolean isCorrect = false;
-                    String answerText = parts[i];
+                List<Answer> answers = new ArrayList<>();
 
-                    for (int j = 6; j < parts.length; j++){
-                        if (parts[j].equals(answerText)){
+                for (int i = 2; i < 6; i++) {
+                    String answerText = parts[i];
+                    boolean isCorrect = false;
+
+                    for (int j = 6; j < parts.length; j++) {
+                        if (parts[j].equals(answerText)) {
                             isCorrect = true;
+                            break;
                         }
                     }
 
-                    Answer answer = new Answer(answerText, i - 2, isCorrect);
+                    Answer answer = new Answer(answerText, i - 1, isCorrect);
                     answers.add(answer);
                 }
-                Question questionObject = new Question(question, answers);
-                questions.add(questionObject);
 
-                if (!oldTopic.equals(questionTopic)){
-                    Quiz quiz = new Quiz(oldTopic, questions, quizOrder);
+                Question questionObject = new Question(question, new ArrayList<>(answers));
+                answers.clear();
+
+                if (quizzes.isEmpty() || quizzes.get(quizzes.size() - 1).getQuestions().size() == 5) {
+                    Quiz quiz = new Quiz(questionTopic, new ArrayList<>(Collections.singletonList(questionObject)), quizOrder);
                     quizzes.add(quiz);
-                    oldTopic = questionTopic;
                     quizOrder++;
+                } else {
+                    quizzes.get(quizzes.size() - 1).getQuestions().add(questionObject);
                 }
             }
         } catch (IOException e) {
