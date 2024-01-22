be927e0032bf21047dfe939e7c5727485552e0b1
1GodLess5 <gacekalbert@seznam.cz>
2024-01-22 08:09:11 +0100

    new class Quiz

---
+++ src/main/java/Quiz.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+    private final String quizTopic;
+    private List<Question> questions = new ArrayList<>();
+    private final int quizOrder;
+
+    public Quiz(String quizTopic, List<Question> questions, int quizOrder) {
+        this.quizTopic = quizTopic;
+        this.questions = questions;
+        this.quizOrder = quizOrder;
+    }
+
+    public String getQuizTopic() {
+        return quizTopic;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public int getQuizOrder() {
+        return quizOrder;
+    }
+}
